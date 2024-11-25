90864e4da6ccc681b87f4e3a07307d727619e184
Victor070656 <ikechukwuv052@gmail.com>
2024-11-25 09:24:02 +0100

    initial commit

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   View,
   BackHandler,
   Button,
+  Image,
 } from "react-native";
 import SafeAreaView from "react-native-safe-area-view";
 import { SafeAreaProvider } from "react-native-safe-area-context";
@@ -14,6 +15,18 @@ import { useEffect, useRef, useState } from "react";
 
 export default function App() {
   const ref = useRef(null);
+  const [networkError, setNetworkError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true); // State to manage splash screen
+
+  useEffect(() => {
+    // Simulate a delay for the splash screen
+    const splashTimeout = setTimeout(() => {
+      setIsLoading(false); // Hide splash screen after 3 seconds
+    }, 10000);
+
+    return () => clearTimeout(splashTimeout);
+  }, []);
+
   useEffect(() => {
     const backHandler = BackHandler.addEventListener(
       "hardwareBackPress",
@@ -21,17 +34,12 @@ export default function App() {
         if (ref.current) {
           ref.current.goBack();
           return true;
-          // prevent default exiting app
         }
         return false;
-        // Default behaviour (exit app if no history or webview)
       }
     );
     return () => backHandler.remove();
-    // cleanup listener on component unmount
   }, []);
-
-  const [networkError, setNetworkError] = useState(false);
 
   const onError = (event) => {
     setNetworkError(true);
@@ -40,6 +48,11 @@ export default function App() {
   const onRefresh = () => {
     setNetworkError(false);
   };
+
+  // Show the splash screen while loading
+  if (isLoading) {
+    return <Splash />;
+  }
 
   return (
     <SafeAreaProvider>
@@ -60,11 +73,7 @@ export default function App() {
               console.log(navState.url);
               ref.current.canGoBack = navState.canGoBack;
             }}
-            startInLoadingState={() => (
-              <View style={{ flex: 1 }}>
-                <ActivityIndicator size={"large"} color={"#00f"} />
-              </View>
-            )}
+            startInLoadingState={() => <Splash />}
             javaScriptEnabled={true}
           />
         )}
@@ -85,6 +94,31 @@ const NetworkErrorScreen = ({ onRefresh }) => {
   );
 };
 
+const Splash = () => {
+  return (
+    <View style={styles.splashContainer}>
+      <Image
+        source={require("./assets/logo.png")}
+        style={styles.splashImage}
+        resizeMode="contain"
+      />
+      <Text
+        style={[styles.splashText1, { marginBottom: 3, fontWeight: "bold" }]}
+      >
+        Welcome To
+      </Text>
+      <Text style={styles.splashText2}>EKUNDAYO</Text>
+      <Text style={styles.splashText3}>EDUCONSULT</Text>
+      <Text style={styles.splashText1}>
+        We are dedicated to providing professionals certifications in various
+        areas of personal development
+      </Text>
+      <View style={{ height: 20 }}></View>
+      <ActivityIndicator size="large" color="#0000ff" />
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -97,8 +131,39 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     rowGap: 10,
   },
+  splashContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#fff",
+  },
+  splashImage: {
+    width: "100%",
+    height: 250,
+    resizeMode: "contain",
+  },
+  splashText1: {
+    fontSize: 18,
+
+    textAlign: "center",
+  },
+  splashText2: {
+    fontSize: 40,
+    lineHeight: 41,
+    fontWeight: "900",
+    textAlign: "center",
+    fontFamily: "serif",
+    color: "#a9cf46",
+  },
+  splashText3: {
+    fontSize: 28,
+    lineHeight: 29,
+    fontWeight: "900",
+    textAlign: "center",
+    fontFamily: "serif",
+  },
   text: {
-    color: "#fff",
+    color: "#000",
     fontSize: 15,
     fontWeight: "800",
   },
